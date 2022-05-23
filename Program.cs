b0cc17241c8a88352b95c7afe9a5e254acc45c4d
Philippe Parisot <pparisot3@gmail.com>
2022-05-22 23:27:17 -0400

    added checklist

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
-﻿
+﻿/* Ask for valid inputs and outputs
+Whatever the interviewer says, write it down
+Develop test cases
+Don’t be afraid to do this out of order
+Don’t be afraid to come back to make more test cases
+Call the function with these conditions, print results
+Ensure your understanding of the problem
+Unless you’re given a signature, write your own signatures
+Start with the first test case
+If you think of other test cases, don’t try to solve it immediately, add it to the bottom of the list of test cases
+Finish all the other test cases, one by one */
+
 
 var test1 = "Mr John Smith";
 var result1 = test1.Replace(" ", "%20");
